9d1004078ac158a10f6dbb1ae96c559f438d6492
Eric Schnell <eric.schnell@rogers.com>
2020-05-15 18:10:39 -0400

    Added more prestige models.

--- network_generator_prestige.py
+++ network_generator_prestige.py
@@ -136,7 +136,7 @@ def human_social_network_decay(grid, geodesic, k, decay_type):
     print(i)
     return G
 
-def human_social_network_exp_decay(grid, geodesic, l):
+def human_social_network_exp_decay(grid, geodesic, l=1):
     """
     Returns a network that mimics human social networks where people try to connect to those who have more connections.
     Connections are only possible between nodes a distance of k apart. Connections between nodes become exponentially
@@ -149,9 +149,9 @@ def human_social_network_exp_decay(grid, geodesic, l):
     geodesic : float
         Desired average shortest path length / degrees of separation (geodesic)
         This will be treated as an upper bound
-    l : [0, 1]
+    l : [0, 1, 2]
         Modifier for exponential function
-        If 1, the clustering coefficient of the graph will be slightly lower
+        Higher values lower the clustering coefficient of the graph
 
     Notes
     -----
@@ -200,22 +200,205 @@ def human_social_network_exp_decay(grid, geodesic, l):
     print(i)
     return G
 
+def human_social_network_exp_decay_all_involve(grid, geodesic):
+    """
+    Returns a network that mimics human social networks where people try to connect to those who have more connections.
+    Connections are only possible between nodes a distance of k apart. Connections between nodes become exponentially
+    less likely the further apart they are from each other.
+
+    Parameters
+    ----------
+    grid : (int, int)
+        Tuple of grid dimensions. Number of nodes in network is grid[0]*grid[1]
+    geodesic : float
+        Desired average shortest path length / degrees of separation (geodesic)
+        This will be treated as an upper bound
+
+    Notes
+    -----
+    The algorithm works by creating a grid where nodes are connected to their neighbors
+    to the North, South, East, and West. The top and bottom of the grid and left and right
+    are connected to form a torus, ensuring all nodes have the same number of edges initially.
+
+    At each iteration one node is selected at random to receive a new edge with a node within a distance of k to them.
+    The node is more likely to connect to a node with many connections and that is nearby. Force all nodes to be
+    selected for a new connection at some point.
+    """
+    # Set up the graph with connections between neighbors
+    G = nx.grid_2d_graph(grid[0], grid[1], True)
+    G = nx.convert_node_labels_to_integers(G)
+    r = int(round((grid[0] * grid[1]) / 10))
+    to_use = []
+
+    i = 0
+    while geodesic < nx.average_shortest_path_length(G):
+        # Only check for desired geodesic every r iterations as the operation is very time consuming
+        for j in range(r):
+            # Select random person
+            if to_use == []:
+                to_use = list(G.nodes)
+            nodes = list(G.nodes)
+            n = random.choice(to_use)
+            to_use.remove(n)
+
+            odds = []
+
+            for optn in nodes:
+                if optn == n:
+                    odds.append(0)
+                else:
+                    dist = nx.shortest_path_length(G, n, optn)
+                    # If the option is a neighbor of n then it cannot be connected to again
+                    if dist == 1:
+                        odds.append(0)
+                    else:
+                        # Set the odds of being connected to based on the distance from n and an exponential decay
+                        # function
+                        # If l is 1, then further nodes will be slightly favoured and thus clustering will slightly
+                        # decrease
+                        w = G.degree(optn) / ((dist - 1) ** (dist - 1))
+                        odds.append(w)
+
+            # Select at random a new connection for n from the list of options given the assigned odds
+            a = random.choices(nodes, weights=odds, k=1)[0]
+            G.add_edge(n, a)
+            i += 1
+
+    print(i)
+    return G
+
+def human_social_network_exp_decay_eigen(grid, geodesic, l=1):
+    """
+    Returns a network that mimics human social networks where people try to connect to those who have more important
+    connections, as calculated using eigenvector centrality. Connections are only possible between nodes a distance of
+    k apart. Connections between nodes become exponentially less likely the further apart they are from each other.
+
+    Parameters
+    ----------
+    grid : (int, int)
+        Tuple of grid dimensions. Number of nodes in network is grid[0]*grid[1]
+    geodesic : float
+        Desired average shortest path length / degrees of separation (geodesic)
+        This will be treated as an upper bound
+    l : [0, 1, 2]
+        Modifier for exponential function
+        Higher values lower the clustering coefficient of the graph
+
+    Notes
+    -----
+    The algorithm works by creating a grid where nodes are connected to their neighbors
+    to the North, South, East, and West. The top and bottom of the grid and left and right
+    are connected to form a torus, ensuring all nodes have the same number of edges initially.
+
+    At each iteration one node is selected at random to receive a new edge with a node within a distance of k to them.
+    The node is more likely to connect to a node with many important connections and that is nearby.
+    """
+    # Set up the graph with connections between neighbors
+    G = nx.grid_2d_graph(grid[0], grid[1], True)
+    G = nx.convert_node_labels_to_integers(G)
+    r = int(round((grid[0] * grid[1]) / 10))
+
+    i = 0
+    while geodesic < nx.average_shortest_path_length(G):
+        # Only check for desired geodesic every r iterations as the operation is very time consuming
+        for j in range(r):
+            # Select random person
+            nodes = list(G.nodes)
+            n = random.choice(nodes)
+            odds = []
+            centrality = nx.eigenvector_centrality_numpy(G)
+
+            for optn in nodes:
+                if optn == n:
+                    odds.append(0)
+                else:
+                    dist = nx.shortest_path_length(G, n, optn)
+                    # If the option is a neighbor of n then it cannot be connected to again
+                    if dist == 1:
+                        odds.append(0)
+                    else:
+                        # Set the odds of being connected to based on the distance from n and an exponential decay
+                        # function
+                        # If l is 1, then further nodes will be slightly favoured and thus clustering will slightly
+                        # decrease
+                        w = centrality[optn] / ((dist) ** (dist - l))
+                        odds.append(w)
+
+            # Select at random a new connection for n from the list of options given the assigned odds
+            a = random.choices(nodes, weights=odds, k=1)[0]
+            G.add_edge(n, a)
+            i += 1
+
+    print(i)
+    return G
+
+def human_social_network_prestige(grid, geodesic):
+    """
+    Returns a network that mimics human social networks where people try to connect to those who have more important
+    connections, as calculated using eigenvector centrality. Connections are only possible between nodes a distance of
+    k apart. Connections between nodes become exponentially less likely the further apart they are from each other.
+
+    Parameters
+    ----------
+    grid : (int, int)
+        Tuple of grid dimensions. Number of nodes in network is grid[0]*grid[1]
+    geodesic : float
+        Desired average shortest path length / degrees of separation (geodesic)
+        This will be treated as an upper bound
+
+    Notes
+    -----
+    The algorithm works by creating a grid where nodes are connected to their neighbors
+    to the North, South, East, and West. The top and bottom of the grid and left and right
+    are connected to form a torus, ensuring all nodes have the same number of edges initially.
+
+    At each iteration one node is selected at random to receive a new edge with a node within a distance of k to them.
+    The node is more likely to connect to a node with many important connections and that is nearby.
+
+    BEST PRESTIGE MODEL AT THE MOMENT
+    """
+    # Set up the graph with connections between neighbors
+    G = nx.grid_2d_graph(grid[0], grid[1], True)
+    G = nx.convert_node_labels_to_integers(G)
+    r = int(round((grid[0] * grid[1]) / 10))
+
+    i = 0
+    while geodesic < nx.average_shortest_path_length(G):
+        # Only check for desired geodesic every r iterations as the operation is very time consuming
+        for j in range(r):
+            # Select random person
+            nodes = list(G.nodes)
+            n = random.choice(nodes)
+            odds = []
+            centrality = nx.eigenvector_centrality_numpy(G)
+
+            for optn in nodes:
+                if optn == n:
+                    odds.append(0)
+                else:
+                    dist = nx.shortest_path_length(G, n, optn)
+                    # If the option is a neighbor of n then it cannot be connected to again
+                    if dist == 1:
+                        odds.append(0)
+                    else:
+                        # Set the odds of being connected to based on the distance from n and an exponential decay
+                        # function
+                        w = centrality[optn] / (dist ** dist)
+                        odds.append(w)
+
+            # Select at random a new connection for n from the list of options given the assigned odds
+            a = random.choices(nodes, weights=odds, k=1)[0]
+            G.add_edge(n, a)
+            i += 1
+
+    print(i)
+    return G
+
 if __name__ == '__main__':
     # Example Networks
 
-    # G = human_social_network_k_algorithm((30, 30), 3.4, 4)
-    # H = human_social_network_decay((30, 30), 3.4, 4, "normal")
-    # I = human_social_network_decay((30, 30), 3.4, 4, "square")
-    # J = human_social_network_decay((30, 30), 3.4, 4, "exponential")
-    # K = human_social_network_exp_decay((30, 30), 3.4, 0)
-    # L = human_social_network_exp_decay((30, 30), 3.4, 1)
-    # M = nx.powerlaw_cluster_graph(900, 3, 0.25)
-    N = human_social_network_exp_decay((15, 15), 3.4, 0)
-    O = human_social_network_exp_decay((15, 15), 3.4, 1)
-    # P = nx.powerlaw_cluster_graph(225, 2, 0.15)
+    G = human_social_network_prestige((15, 15), 3.4)
 
-    degree_distribution_plot(N)
-    statistics(N)
-    degree_distribution_plot(O)
-    statistics(O)
+    degree_distribution_plot(G)
+    statistics(G)
     plt.show()
